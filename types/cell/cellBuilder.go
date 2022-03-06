8248aef484545b9b71c3db79c7eb8dd91c9fe0d8
Geoff Ford <gford1000@hotmail.com>
2022-03-06 18:58:19 +0000

    Switched to NewEncryptableValue func

--- types/cell/cellBuilder.go
+++ types/cell/cellBuilder.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 
 	"github.com/gford1000-go/protobuf/types/encryptable_value"
-	"github.com/gford1000-go/protobuf/types/encrypted_object"
 	"github.com/gford1000-go/protobuf/types/encryption"
 	"github.com/gford1000-go/protobuf/types/value"
 	"google.golang.org/protobuf/proto"
@@ -25,40 +24,6 @@ type CellBuilder struct {
 	e encryption.TokenKeyEncryptor
 }
 
-func (cb *CellBuilder) marshalValue(v *value.Value) ([]byte, error) {
-	data, err := proto.Marshal(v)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
-func (cb *CellBuilder) encryptValue(keyToken []byte, v *value.Value) (*encrypted_object.EncryptedObject, error) {
-
-	b, err := cb.marshalValue(v)
-	if err != nil {
-		return nil, err
-	}
-
-	e, a, err := cb.e.Encrypt(keyToken, b)
-	if err != nil {
-		return nil, err
-	}
-
-	algo, err := encryption.NewAlgo(a)
-	if err != nil {
-		return nil, err
-	}
-
-	return &encrypted_object.EncryptedObject{
-			A:        algo,
-			KeyToken: keyToken,
-			V:        e,
-		},
-		err
-}
-
 func (cb *CellBuilder) Marshal(i interface{}, d TokenKeyRetriever, ve ValueEncryptionDeterminer) ([]byte, error) {
 
 	if d == nil {
@@ -77,32 +42,12 @@ func (cb *CellBuilder) Marshal(i interface{}, d TokenKeyRetriever, ve ValueEncry
 	var keyToken []byte = d.GetToken()
 	var encrypt bool = ve.Encrypt(keyToken)
 
-	var e *encrypted_object.EncryptedObject
-
-	if encrypt {
-		var err error
-		e, err = cb.encryptValue(keyToken, v)
-		if err != nil {
-			return nil, err
-		}
+	e, err := encryptable_value.NewEncryptableValue(keyToken, v, encrypt, cb.e)
+	if err != nil {
+		return nil, err
 	}
 
-	var c *Cell
-	if encrypt {
-		c = &Cell{C: &Cell_Scalar{
-			Scalar: &encryptable_value.EncryptableValue{
-				C: &encryptable_value.EncryptableValue_E{E: e},
-			},
-		},
-		}
-	} else {
-		c = &Cell{C: &Cell_Scalar{
-			Scalar: &encryptable_value.EncryptableValue{
-				C: &encryptable_value.EncryptableValue_V{V: v},
-			},
-		},
-		}
-	}
+	c := &Cell{C: &Cell_Scalar{Scalar: e}}
 
 	data, err := proto.Marshal(c)
 	if err != nil {
--- types/cell/go.mod
+++ types/cell/go.mod
@@ -3,9 +3,10 @@ module github.com/gford1000-go/protobuf/types/cell
 go 1.17
 
 require (
-	github.com/gford1000-go/protobuf/types/encryptable_value v0.0.0-20220306094744-c2dfe6be4dc5
-	github.com/gford1000-go/protobuf/types/encrypted_object v0.0.0-20220306094632-a05793ec1e8d
-	github.com/gford1000-go/protobuf/types/encryption v0.0.0-20220306094250-5a234c387651
-	github.com/gford1000-go/protobuf/types/value v0.0.0-20220306094744-c2dfe6be4dc5
+	github.com/gford1000-go/protobuf/types/encryptable_value v0.0.0-20220306185007-26942c67ca7f
+	github.com/gford1000-go/protobuf/types/encryption v0.0.0-20220306183545-e203b7438816
+	github.com/gford1000-go/protobuf/types/value v0.0.0-20220306185007-26942c67ca7f
 	google.golang.org/protobuf v1.27.1
 )
+
+require github.com/gford1000-go/protobuf/types/encrypted_object v0.0.0-20220306181137-d8a7a4967def // indirect
