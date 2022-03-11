d3ec70a632cb2ee6a4ec1de2b81768631dac2302
Geoff Ford <gford1000@hotmail.com>
2022-03-11 19:03:15 +0000

    Extended to TokenKeyEncryptor interface to include GetKeys()

--- types/encryption/go.mod
+++ types/encryption/go.mod
@@ -3,6 +3,6 @@ module github.com/gford1000-go/protobuf/types/encryption
 go 1.17
 
 require (
-	github.com/gford1000-go/protobuf/types/value v0.0.0-20220310071951-94ce62209d95
+	github.com/gford1000-go/protobuf/types/value v0.0.0-20220311184423-32b6fd6de6c3
 	google.golang.org/protobuf v1.27.1
 )
--- types/encryption/tokenKeyEncryptor.go
+++ types/encryption/tokenKeyEncryptor.go
@@ -2,6 +2,10 @@ package encryption
 
 // TokenKeyEncryptor receives a token value which is used to retrieve
 // the key required to encrypt.
+//
+// The GetKeys function uses the provided key to encrypt the map
+// of tokens->keys inside an EncryptedObject, for secure distribution
 type TokenKeyEncryptor interface {
 	EncryptFromToken(token []byte, plaintext []byte) ([]byte, Algorithm, error)
+	GetKeys(key []byte) (*EncryptedObject, error)
 }
