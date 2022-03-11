d28ae1354109b4bf79d4f4ec857e4872833799c5
Geoff Ford <gford1000@hotmail.com>
2022-03-11 05:33:22 +0000

    Consolidated encrypted_object into encryption

---
+++ types/encryption/encryptedObjectParser.go
@@ -0,0 +1,50 @@
+package encryption
+
+import (
+	"errors"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+var errMissingDecryptor = errors.New("TokenKeyDecryptor must not be nil")
+var errNilMessage = errors.New("message must not be nil")
+
+// EncryptedObjectParser decrypts EncryptedObjects in a Message, using
+// the supplied TokenKeyDecryptor
+func NewEncryptedObjectParser(decryptor TokenKeyDecryptor) (*EncryptedObjectParser, error) {
+	if decryptor == nil {
+		return nil, errMissingDecryptor
+	}
+
+	return &EncryptedObjectParser{d: decryptor}, nil
+}
+
+type EncryptedObjectParser struct {
+	d TokenKeyDecryptor
+}
+
+// Parse decrypts using into the supplied ProtoMessage instance
+func (cp *EncryptedObjectParser) Parse(e *EncryptedObject, message protoreflect.ProtoMessage) error {
+
+	if message == nil {
+		return errNilMessage
+	}
+
+	a, err := ParseAlgo(e.A)
+	if err != nil {
+		return err
+	}
+
+	b, err := cp.d.Decrypt(e.GetKeyToken(), a, e.V)
+	if err != nil {
+		return err
+	}
+
+	err = proto.Unmarshal(b, message)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- types/encryption/go.mod
+++ types/encryption/go.mod
@@ -2,4 +2,7 @@ module github.com/gford1000-go/protobuf/types/encryption
 
 go 1.17
 
-require google.golang.org/protobuf v1.27.1 // indirect
+require (
+	github.com/gford1000-go/protobuf/types/value v0.0.0-20220310071951-94ce62209d95
+	google.golang.org/protobuf v1.27.1
+)
