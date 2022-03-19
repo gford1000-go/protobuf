79d3eafdd482be6555949701b7c477b487bb8a6e
Geoff Ford <gford1000@hotmail.com>
2022-03-19 11:34:28 +0000

    Parse/NewAlgo are extensible

--- types/encryption/algo.go
+++ types/encryption/algo.go
@@ -4,26 +4,39 @@ import "errors"
 
 var errUnknownAlgorithmUsed = errors.New("unsupported algorithm used for encryption")
 
+var algoMap1 map[AlgoType]Algo
+var algoMap2 map[Algo]AlgoType
+
+func init() {
+	algoMap1 = make(map[AlgoType]Algo)
+	algoMap2 = make(map[Algo]AlgoType)
+
+	RegisterAlgoMapping(Algo_GCM, GCM)
+}
+
+// RgisterAlgoMapping provides the ability to specify new
+// mappings between the proto definition and go code
+func RegisterAlgoMapping(a Algo, at AlgoType) {
+	algoMap1[at] = a
+	algoMap2[a] = at
+}
+
 // NewAlgo returns the corresponding Algo to the AlgoType,
 // or returns Algo_Unknown and an error if not matched
-func NewAlgo(a AlgoType) (Algo, error) {
-	switch a {
-	case GCM:
-		{
-			return Algo_GCM, nil
-		}
+func NewAlgo(at AlgoType) (Algo, error) {
+	a, ok := algoMap1[at]
+	if !ok {
+		return Algo_UnknownAlgo, errUnknownAlgorithmUsed
 	}
-	return Algo_UnknownAlgo, errUnknownAlgorithmUsed
+	return a, nil
 }
 
 // ParseAlgo returns the corresponding AlgoType to the Algo,
 // or returns Unknown and an error if not matched
 func ParseAlgo(a Algo) (AlgoType, error) {
-	switch a {
-	case Algo_GCM:
-		{
-			return GCM, nil
-		}
+	at, ok := algoMap2[a]
+	if !ok {
+		return Unknown, errUnknownAlgorithmUsed
 	}
-	return Unknown, errUnknownAlgorithmUsed
+	return at, nil
 }
